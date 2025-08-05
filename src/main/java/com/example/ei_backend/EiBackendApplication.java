241be6ca165dc7a7a1121711f79990d306d2eabd
Mincheol Ha <hmc0501@gmail.com>
2025-08-05 14:02:26 +0900

    오타 수정

--- src/main/java/com/example/ei_backend/EiBackendApplication.java
+++ src/main/java/com/example/ei_backend/EiBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EiBackendApplicatio {
+public class EiBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EiBackendApplicatio.class, args);
+        SpringApplication.run(EiBackendApplication.class, args);
     }
 
 }
