d25d4e16154ec2a96a37c640131daf2e982a5cb0
Mincheol Ha <hmc0501@gmail.com>
2025-09-12 13:53:37 +0900

    채팅시간 아시아/서울 기준으로 수정

--- src/main/java/com/example/ei_backend/domain/dto/chat/ChatMessageResponse.java
+++ src/main/java/com/example/ei_backend/domain/dto/chat/ChatMessageResponse.java
@@ -18,6 +18,10 @@ public class ChatMessageResponse {
     private String senderEmail;
     private String message;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd'T'HH:mm:ss",
+            timezone = "Asia/Seoul"
+    )
     private LocalDateTime sentAt;
 }
